159e0f49ba50b303ec983fe5fe305b2385072188
afton <aaike001@gmail.com>
2016-03-03 16:43:00 -0800

    add folders src views tests and update files

--- app/app.php
+++ app/app.php
@@ -1,8 +1,22 @@
 <?php
 
 		require_once __DIR__."/../vendor/autoload.php";
+		require_once __DIR__."/../src/Author.php";
+	 	require_once __DIR__."/../src/Book.php";
 
 		$app = new Silex/Application();
+
+		$server = 'mysql:host=localhost;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+  	Request::enableHttpMethodParameterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
 
 		$app->get("/", function() {
 			return "home";
