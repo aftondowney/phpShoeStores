af6aeaa43edf51c2bb9eb96a5f920b5ab2325962
Afton Downey <aaike001@gmail.com>
2016-03-04 11:35:09 -0800

    add routes for brands

--- app/app.php
+++ app/app.php
@@ -22,7 +22,7 @@
 				return $app['twig']->render('index.html.twig');
 		});
 
-		//view/update/delete routes for Store
+		//view/update/delete routes for stores
 		$app->get("/stores", function() use ($app) {
 				return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'form' => false));
 		});
@@ -54,6 +54,36 @@
 		$app->post("/delete_stores", function() use ($app) {
         Store::deleteAll();
         return $app['twig']->render('index.html.twig');
+    });
+
+		//routes to view individual store and add brands
+		$app->get("/store/{id}", function($id) use ($app) {
+        $store = Store::findStoreById($id);
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store -> getBrands(), 'all_brands' => Brand::getAll()));
+    });
+
+		$app->post("/add_brands", function() use ($app) {
+        $store = Store::findStoreById($_POST['store_id']);
+        $brand = Brand::findBrandById($_POST['brand_id']);
+        $store->addBrand($brand);
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
+      });
+
+			//routes to view/add/delete brands
+		$app->get("/brands", function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'form' => false));
+    });
+
+    $app->post("/brands", function() use ($app) {
+        $name = $_POST['name'];
+        $new_brand = new Brand($name);
+        $new_brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'form' => false));
+    });
+
+    $app->get("/brand/{id}", function($id) use ($app) {
+        $brand = Brand::findBrandById($id);
+        return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'brands' => $brand -> getStores(), 'all_stores' => Store::getAll(), 'stores' => $brand->getStores()));
     });
 
 
