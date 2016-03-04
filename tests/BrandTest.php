26a907e2ecd6f2041ff8efd794452066855afd7e
Afton Downey <aaike001@gmail.com>
2016-03-04 09:29:40 -0800

    pass save test for Brand

---
+++ tests/BrandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Store.php";
+    require_once "src/Brand.php";
+
+
+    $server = 'mysql:host=localhost;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Store::deleteAll();
+            // Brand::deleteAll();
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $name = "Naturalizer";
+            $id = null;
+            $test_brand = new Brand($name, $id);
+            $test_brand->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals($test_brand, $result[0]);
+        }
+
+    }
+?>
